75a70e0dbb1118163d80c23ded0d6c339cce3ef6
ventura.paolo <ventura.paolo@gmail.com>
2018-09-15 22:19:04 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,6 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colours)
-  puts
+  puts "R: #{colours[0]}, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
 
-"R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
